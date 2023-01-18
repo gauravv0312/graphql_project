b39c6867060772bc9f85f513a5285243efa6dddb
gauravv0312 <gauravv0312@gmail.com>
2023-01-18 15:47:26 +0530

    manage the files and folder

---
+++ Schemas/index.js
@@ -0,0 +1,44 @@
+const graphql = require("graphql");
+
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema } = graphql
+
+ const UserType = require('./TypeDefs/UserType');
+ const userData = require("../MOCK_DATA.json");
+
+// RootQuery describes how users can use the graph and grab data and all the query to get all user and etc.
+const RootQuery = new GraphQLObjectType({
+    name : "RootQueryType",
+    fields:{
+       getAllUsers:{
+        type: new graphql.GraphQLList(UserType),
+        args: { id:{type: GraphQLInt}},  // we passing the argument for get data by id
+        resolve(parent,args){
+            return userData
+        }
+       }
+    }
+});
+
+const Mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields:{
+        createUser:{
+            type:UserType,
+            args:{
+                first_name: {type : GraphQLString},
+                last_name: {type : GraphQLString},
+                email: {type : GraphQLString},
+                gender: {type : GraphQLString},
+            },
+            resolve(parent,args){
+                userData.push({id :userData.length+1,first_name:args.first_name,last_name:args.last_name,email:args.email,gender:args.gender})
+                return args
+            }
+        }
+    }
+})
+
+module.exports= new GraphQLSchema({
+    query: RootQuery,
+    mutation: Mutation
+});
--- index.js
+++ index.js
@@ -1,59 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 4000;
-const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema } = graphql
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
-const userData = require('./MOCK_DATA.json');
 
-
-const UserType = new GraphQLObjectType({
-    name: "User",
-    fields: () => ({
-        id: {type : GraphQLInt},
-        first_name: {type : GraphQLString},
-        last_name: {type : GraphQLString},
-        email: {type : GraphQLString},
-        gender: {type : GraphQLString}
-    })
-})
-// RootQuery describes how users can use the graph and grab data and all the query to get all user and etc.
-const RootQuery = new GraphQLObjectType({
-    name : "RootQueryType",
-    fields:{
-       getAllUsers:{
-        type: new graphql.GraphQLList(UserType),
-        args: { id:{type: GraphQLInt}},  // we passing the argument for get data by id
-        resolve(parent,args){
-            return userData
-        }
-       }
-    }
-});
-
-const Mutation = new GraphQLObjectType({
-    name: "Mutation",
-    fields:{
-        createUser:{
-            type:UserType,
-            args:{
-                first_name: {type : GraphQLString},
-                last_name: {type : GraphQLString},
-                email: {type : GraphQLString},
-                gender: {type : GraphQLString},
-            },
-            resolve(parent,args){
-                userData.push({id :userData.length+1,first_name:args.first_name,last_name:args.last_name,email:args.email,gender:args.gender})
-                return args
-            }
-        }
-    }
-})
-
-const schema = new GraphQLSchema({
-    query: RootQuery,
-    mutation: Mutation
-})
+const  schema = require('./Schemas/index')
 
 //This route will be used as an endpoint to interact with Graphql, and All queries will go through this route
 app.use('/graphql', graphqlHTTP({
